3082ebd70d0eb273ca47912ee1dab8570c057f12
Chris Tsui <chris.tsui112@outlook.com>
2020-08-17 10:17:30 -0700

    added display winner and display computer choice

--- code.js
+++ code.js
@@ -10,7 +10,6 @@ let player = { choice: ""}
                     computerWins:"You lose!",
                     tie:"You Tie!"
     };
-
     let computer = {choice:"",
         selection: function(){
         let rockPaperScissorsDict = {0: "Rock",
@@ -19,17 +18,28 @@ let player = { choice: ""}
         }
         let generateSelection = Math.floor((Math.random() * 3));
         return rockPaperScissorsDict[generateSelection];
-
     }};
     let buttonNodeList = document.querySelectorAll("button");
     for (let i = 0; i < buttonNodeList.length; i++){
         buttonNodeList[i].addEventListener('click',event=>{
             player.choice = event.target.id;
-            computer.choice = computer.selection()
-            if (computer.choice.toLowerCase() === player.choice){
-                alert(messages.tie);
-            }else {
-            let winner = whoWinsDict[player.choice+computer.choice];
-                alert(messages[winner]);
-    }
+            computer.choice = computer.selection();
+            displayWinner();
+
         })};
+    function displayWinner(){
+        const img = document.getElementById("computerThrows");
+        switch(computer.choice){
+            case "Rock":
+                img.setAttribute('src',"rock.png");
+                break;
+            case "Paper":
+                img.setAttribute('src',"paper.png");
+                break;
+            case "Scissors":
+                img.setAttribute('src',"scissors.png");
+                break;
+        }
+        let winner = whoWinsDict[player.choice+computer.choice];
+            (computer.choice.toLowerCase() === player.choice)? alert(messages.tie):alert(messages[winner]);
+    }
